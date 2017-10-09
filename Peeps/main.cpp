b1f4587b1a9c0d1c23ef1ae8b9bcee9c3c467eb5
William Wayland <william.wayland@griffithuni.edu.au>
2017-10-09 23:02:31 +1000

    Initial Commit

---
+++ Peeps/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Peeps
+//
+//  Created by William Wayland on 9/10/17.
+//  Copyright © 2017 William Wayland. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
