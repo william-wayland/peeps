b42d4b5213ce068c0d88f849b45d45f15a6b7fa7
William Wayland <william.wayland@griffithuni.edu.au>
2018-08-12 00:38:39 +1000

    Added multiple forks option in code. Defaults to two.

--- Peeps/main.cpp
+++ Peeps/main.cpp
@@ -31,7 +31,7 @@ public:
   forks_till_full_(forks_till_full)
   {
   }
-
+  
   // Eat away kiddo
   void eat()
   {
@@ -40,26 +40,26 @@ public:
       std::unique_lock<std::mutex> guard_r(forks_[right_], std::defer_lock);
       std::unique_lock<std::mutex> guard_l(forks_[left_], std::defer_lock);
       std::lock(guard_r, guard_l);
-
+      
       write_guard_.lock();
       std::cout << name_ << " has started eating..." << std::endl;
       write_guard_.unlock();
-
+      
       // Eating.. yum!
       std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
+      
       write_guard_.lock();
       std::cout << name_ << " has finished eating..." << std::endl;
       write_guard_.unlock();
     }
   }
-
+  
   // Spawns a thread for this object to eat...
   std::thread spawn()
   {
     return std::thread(&Peep::eat, this);
   }
-
+  
 private:
   int left_, right_;
   const std::string& name_;
@@ -71,31 +71,32 @@ private:
 int main() {
   std::array<std::mutex, 5> forks;
   std::mutex write_guard;
-
+  
   const std::array<std::string, 5> names =
   { "Karl Marx", "Lenin", "Fidel Castro", "Xi Jinping", "Leon Trotsky" };
-
+  
+  const int forks_till_fill = 2;
   std::array<Peep, 5> peeps =
   {
-    Peep(0, 1, names[0], forks, write_guard, 3),
-    Peep(1, 2, names[1], forks, write_guard, 3),
-    Peep(2, 3, names[2], forks, write_guard, 3),
-    Peep(3, 4, names[3], forks, write_guard, 3),
-    Peep(4, 0, names[4], forks, write_guard, 3)
+    Peep(0, 1, names[0], forks, write_guard, forks_till_fill),
+    Peep(1, 2, names[1], forks, write_guard, forks_till_fill),
+    Peep(2, 3, names[2], forks, write_guard, forks_till_fill),
+    Peep(3, 4, names[3], forks, write_guard, forks_till_fill),
+    Peep(4, 0, names[4], forks, write_guard, forks_till_fill)
   };
-
+  
   std::vector<std::thread> chairs;
   for (auto& peep : peeps)
   {
     chairs.push_back(peep.spawn());
   }
-
+  
   for (auto& chair : chairs)
   {
     chair.join();
   }
-
+  
   std::cout << "Finished..." << std::endl;
-
+  
   return 0;
 }
